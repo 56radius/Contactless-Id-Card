7bfa936324d6a51464cfb2a155a24344f273aeb7
56radius <cussmeritmohammed@gmail.com>
2023-07-26 01:53:54 +0100

    Update

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,7 +2,7 @@ import React from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function SignupScreen({ navigation }) {
@@ -11,8 +11,24 @@ export default function SignupScreen({ navigation }) {
       style={styles.container}
       colors={["#9F9F9F", "#4E4E4E", "#000000"]}
     >
-      <View>
-        <Text> hehe </Text>
+      <View style={{ flex: 0.7 }}>
+        <Image
+          style={{
+            opacity: 0.5,
+            width: 148,
+            height: 148,
+            position: "absolute",
+            top: -39,
+            alignSelf: "center",
+          }}
+          source={require("../.././assets/mtu.png")}
+        />
+        <Text style={{ color: "#fff", fontSize: 35 }}>
+          This is Contactless ID
+        </Text>
+        <Text style={{ color: "#fff", fontSize: 12, fontWeight: "bold" }}>
+          Welcome to our software, new user
+        </Text>
       </View>
     </LinearGradient>
   );
